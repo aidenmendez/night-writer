7c88b6cb50c21697ff44942a3a1c0a32a0e561b2
Aiden Mendez <aidenmendez@gmail.com>
2020-11-05 19:14:06 -0700

    Set up night writer test file and test night_writer exists

---
+++ test/night_writer_test.rb
@@ -0,0 +1,12 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/night_writer'
+
+class NightWriterTest < Minitest::Test
+  def setup
+    @night_writer = NightWriter.new
+  end
+  def test_it_exists	
+    assert_instance_of NightWriter, @night_writer 
+  end
+end
