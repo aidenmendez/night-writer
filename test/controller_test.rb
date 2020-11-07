74cb4d59f1fa6f790c0bfbd2c67d72d28b5dbbe7
Aiden Mendez <aidenmendez@gmail.com>
2020-11-06 18:34:24 -0700

    Test controller returns a message

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -4,10 +4,16 @@ require './lib/controller'
 
 class ControllerTest < Minitest::Test
   def setup
-    @controller = Controller.new
+    @controller = Controller.new(["message.txt", "braille.txt"])
   end
 
   def test_it_exists	
     assert_instance_of Controller, @controller
   end
+
+  def test_it_returns_a_message
+    msg = "Created 'braille.txt' containing 256 characters"
+    assert_equal msg, @controller.respond_to_user
+  end
+  
 end
