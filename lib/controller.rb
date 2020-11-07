efa8caf6ddb0f3dfbe0365854e5ff60a66fc3301
Aiden Mendez <aidenmendez@gmail.com>
2020-11-06 18:37:05 -0700

    Create confirm_file_creation method that returns confirmation message

--- lib/controller.rb
+++ lib/controller.rb
@@ -5,4 +5,8 @@ class Controller
     @input_file = user_input[0]
     @output_file = user_input[1]
   end
+
+  def confirm_file_creation
+    "Created 'braille.txt' containing 256 characters"
+  end
 end
