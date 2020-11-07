ece44783ae9c9511094ce0e8cd4c8f039b1ab1a5
Aiden Mendez <aidenmendez@gmail.com>
2020-11-06 19:17:24 -0700

    Update confirm_file_creation method to change based on user input and file size

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,10 +4,11 @@ class Controller
   def initialize(user_input)
     @input_file_name = user_input[0]
     @output_file_name = user_input[1]
+    @input_file_content = read_in_file(input_file_name)
   end
 
   def confirm_file_creation
-    "Created '#{@output_file_name}' containing 256 characters"
+    "Created '#{@output_file_name}' containing #{@input_file_content.length} characters"
   end
 
   def read_in_file(input)
