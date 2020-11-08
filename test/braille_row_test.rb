d864354181ff503e87644f3383ea27152d2453ac
Aiden Mendez <aidenmendez@gmail.com>
2020-11-07 19:50:34 -0700

    Test it has attributes

--- test/braille_row_test.rb
+++ test/braille_row_test.rb
@@ -6,7 +6,8 @@ class BrailleRowTest < Minitest::Test
     @braille_row = BrailleRow.new(sample_text)
   end
 
-  def test_it_exists	
-    assert_instance_of BrailleRow, @braille_row 
+  def test_it_exists_and_has_attributes
+    assert_instance_of BrailleRow, @braille_row
+    assert_equal "w", @braille_row.characters
   end
 end
