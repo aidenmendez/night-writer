00c8a0c5a801455451267a4983020eb2648dd7de
Aiden Mendez <aidenmendez@gmail.com>
2020-11-07 19:09:49 -0700

    Add write_output_file method

--- lib/writer_manager.rb
+++ lib/writer_manager.rb
@@ -4,4 +4,14 @@ class WriterManager
   def initialize
     @braille_rows = []
   end
+
+  def get_file_content(file_name)
+    File.readlines(file_name)[0]
+  end
+
+  def write_output_file(content, file_name)
+    file = File.open(file_name, "w")
+    file.write(content)
+    file.close
+  end
 end
