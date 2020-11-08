a83d00a2d792df9edb621e96ab78400e113b2469
Aiden Mendez <aidenmendez@gmail.com>
2020-11-07 22:41:46 -0700

    Test build_lines method

--- test/braille_row_test.rb
+++ test/braille_row_test.rb
@@ -2,12 +2,24 @@ require_relative "./test_helper"
 
 class BrailleRowTest < Minitest::Test
   def setup
-    sample_text = "w" #elcome to my program!
-    @braille_row = BrailleRow.new(sample_text)
+    parent = mock
+    @controller = Controller.new(["message.txt", "braille.txt"])
+    @braille_row = @controller.writer_manager.braille_rows[0]
   end
 
   def test_it_exists_and_has_attributes
     assert_instance_of BrailleRow, @braille_row
-    assert_equal "w", @braille_row.characters
+    assert_equal "welcome to my program!", @braille_row.characters
+  end
+
+  def test_it_builds_lines
+    parent = mock
+    test_row = BrailleRow.new(parent, "w")
+
+    test_row.build_lines("we")
+
+    assert_equal ".00.", test_row.line_1
+    assert_equal "00.0", test_row.line_2
+    assert_equal ".0..", test_row.line_3
   end
 end
