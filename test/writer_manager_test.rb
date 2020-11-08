0ee2d5b059f1936bba2d4b5548d76b3abf091dfc
Aiden Mendez <aidenmendez@gmail.com>
2020-11-07 18:17:38 -0700

    Test it has attributes

--- test/writer_manager_test.rb
+++ test/writer_manager_test.rb
@@ -5,7 +5,9 @@ class WriterManagerTest < Minitest::Test
     @writer_manager = WriterManager.new
   end
 
-  def test_it_exists	
+  def test_it_exists_and_has_atttributes
     assert_instance_of WriterManager, @writer_manager 
+    assert_equal [], @writer_manager.braille_rows
   end
+
 end
