3f34758f97d6a1e578c2695dc0bcee21d96b4468
Aiden Mendez <aidenmendez@gmail.com>
2020-11-07 18:58:38 -0700

    Move file_content_method and update test

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,23 +1,23 @@
 require "./lib/responder"
+require "./lib/writer_manager"
 
 class Controller
   attr_reader :input_file_name, :output_file_name, :input_file_content
 
-  
-
   def initialize(user_input)
     @input_file_name = user_input[0]
     @output_file_name = user_input[1]
-    @input_file_content = get_file_content(input_file_name)
     @responder = Responder.new
+    @writer_manager = WriterManager.new
+    @input_file_content = get_file_content
   end
 
   def confirm_file_created
    @responder.confirm_file_created(@output_file_name, @input_file_content.length)
   end
 
-  def get_file_content(file_name)
-    File.readlines(file_name)[0]
+  def get_file_content
+    @writer_manager.get_file_content(input_file_name)
   end
 
   def write_output_file(content)
