1da9978f1f9e795cf04f431d7e52e925a7823b44
Aiden Mendez <aidenmendez@gmail.com>
2020-11-07 18:25:47 -0700

    Test writer manager can get input file content

--- test/writer_manager_test.rb
+++ test/writer_manager_test.rb
@@ -10,4 +10,9 @@ class WriterManagerTest < Minitest::Test
     assert_equal [], @writer_manager.braille_rows
   end
 
+  def test_it_can_get_file_contents
+    text = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis lorem lectus, vel interdum lorem imperdiet quis. Proin aliquam urna eu leo mollis, et laoreet lorem viverra. Vivamus blandit enim ipsum, nec luctus risus maximus vel. In sed mi odiom test"
+    assert_equal text, @writer_manager.get_file_content("message.txt")
+  end
+
 end
