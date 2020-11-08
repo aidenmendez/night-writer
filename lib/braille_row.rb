8d42426f94f81292718456b7f652b7e2427a8acc
Aiden Mendez <aidenmendez@gmail.com>
2020-11-08 12:07:42 -0700

    Build lines during initialize Add space translation to translator class key

--- lib/braille_row.rb
+++ lib/braille_row.rb
@@ -2,7 +2,7 @@ class BrailleRow
   attr_reader :characters,
               :line_1,
               :line_2,
-              :qqqline_3
+              :line_3
 
   def initialize(parent, characters)
     @parent = parent
@@ -10,13 +10,24 @@ class BrailleRow
     @line_1 = ""
     @line_2 = ""
     @line_3 = ""
+    build_lines(@characters)
   end
 
   def build_lines(characters)
     chars = characters.scan(/.{1,1}/)
+    chars.each do |char|
+      braille_arr = translate_char(char)
+      concat_lines(braille_arr)
+    end
   end
 
   def translate_char(char)
     @parent.translate_char(char)
+  end
+
+  def concat_lines(braille_arr)
+    @line_1 << braille_arr[0]
+    @line_2 << braille_arr[1]
+    @line_3 << braille_arr[2]
   end
 end
