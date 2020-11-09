8c9f212996764491fbf009d2f53f6bfe6aeaebb5
Aiden Mendez <aidenmendez@gmail.com>
2020-11-09 12:10:18 -0700

    Add function parameter to writer runner file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,5 @@
 require "./lib/controller"
 
 user_input = ARGV
-controller = Controller.run(user_input)
+controller = Controller.run(user_input, "writer")
 puts controller.confirm_file_created(controller.output_file_name, controller.writer_manager.text.length)
