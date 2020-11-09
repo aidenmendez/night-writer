7c8c5ebbb7d4d3cea18779b8653fb23d7f52e901
Aiden Mendez <aidenmendez@gmail.com>
2020-11-09 15:59:21 -0700

    convert input_file_content array to string

--- lib/reader_manager.rb
+++ lib/reader_manager.rb
@@ -1,5 +1,9 @@
-class ReaderManager
-  def initialize(parent, input_file)
-  
+require './lib/manager'
+require "./lib/manager"
+
+class ReaderManager < Manager
+  def initialize(parent, user_input)
+    super(parent, user_input)
+    @input_file_content = @input_file_content.join.gsub("\n", "")
   end
 end
