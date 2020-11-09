ebc5cc5a8e31b7f546a0ebecf2c6eab4773c48f7
Aiden Mendez <aidenmendez@gmail.com>
2020-11-09 15:37:52 -0700

    Test it inherits attributes and stores braille string

--- test/reader_manager_test.rb
+++ test/reader_manager_test.rb
@@ -6,14 +6,24 @@ require './lib/controller'
 class ReaderManagerTest < Minitest::Test
   def setup
     locations = {
-      input: "./test/fixture_files/braille.txt",
-      output: "./test/fixture_files/output/en_message.txt"
+      input: "./test/fixture_files/short_braille_sample.txt",
+      output: "./test/fixture_files/output/eng_message.txt"
     }
-    @controller = Controller.new([locations[:input], locations[:output]], "reader")
-    @reader_manager = controller.manager
+
+    @reader_manager = ReaderManager.new(nil, [locations[:input], locations[:output]])
   end
 
   def test_it_exists
     assert_instance_of ReaderManager, @reader_manager
+  end
+
+  def test_it_inherits_attributes
+    braille = ".00.0.000.000....00...0000..000.0.000.0.00..00.00....0...0..00.0.....0..0.00.00000....00.0..0...0.0.....0.0...0.00..0.0.0...0...0.0."
+
+    assert_equal "./test/fixture_files/short_braille_sample.txt",  @reader_manager.input_file_name
+    assert_equal "./test/fixture_files/output/eng_message.txt",  @reader_manager.output_file_name
+    assert_instance_of Translator, @reader_manager.translator
+    assert_equal braille, @reader_manager.input_file_content
+
   end
 end
