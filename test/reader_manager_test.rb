434bb2edf83507547d5d02c72add9e2e4490d331
Aiden Mendez <aidenmendez@gmail.com>
2020-11-09 11:34:52 -0700

    Set up reader manager test and rename file

---
+++ test/reader_manager_test.rb
@@ -0,0 +1,19 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/reader_manager'
+require './lib/controller'
+
+class ReaderManagerTest < Minitest::Test
+  def setup
+    locations = {
+      input: "./test/fixture_files/braille.txt",
+      output: "./test/fixture_files/en_message.txt"
+    }
+    @controller = Controller.new([locations[:input], locations[:output]], "reader")
+    @reader_manager = controller.manager
+  end
+
+  def test_it_exists
+    assert_instance_of ReaderManager, @reader_manager
+  end
+end
