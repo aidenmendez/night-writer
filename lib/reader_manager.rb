ece661472d8deb43c550914656c5759042a17c53
Aiden Mendez <aidenmendez@gmail.com>
2020-11-09 16:40:58 -0700

    Remove unnecessary input_file_content manipulation

--- lib/reader_manager.rb
+++ lib/reader_manager.rb
@@ -4,6 +4,9 @@ require "./lib/manager"
 class ReaderManager < Manager
   def initialize(parent, user_input)
     super(parent, user_input)
-    @input_file_content = @input_file_content.join.gsub("\n", "")
+  end
+
+  def count_chars
+    
   end
 end
