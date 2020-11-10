83e360d43ddfb9fdda495baa7096b4679c4a035a
Aiden Mendez <aidenmendez@gmail.com>
2020-11-09 20:32:21 -0700

    Test it can write output file

--- test/reader_manager_test.rb
+++ test/reader_manager_test.rb
@@ -61,9 +61,25 @@ class ReaderManagerTest < Minitest::Test
     assert_equal "welcome to my ", @reader_manager.translate_keys(keys)
   end
 
-  def test_it_can_break_braille_array_into_rows
+  def test_it_can_slice_braille_array_into_rows
     single_row =  [".00.0.000.000....00...0000..000.0.000.0.00..\n", "00.00....0...0..00.0.....0..0.00.00000....00\n", ".0..0...0.0.....0.0...0.00..0.0.0...0...0.0."]
-    all_lines = @reader_manager.input_file_content
-    assert_equal single_row, @reader_manager.slice_rows(all_lines)
+
+    assert_equal [single_row], @reader_manager.slice_into_rows(single_row)
+  end
+
+  def test_collect_translation_keys_zzz
+    skip
+    single_row =  [".00.0.000.000....00...0000..000.0.000.0.00..\n", "00.00....0...0..00.0.....0..0.00.00000....00\n", ".0..0...0.0.....0.0...0.00..0.0.0...0...0.0."]
+    require 'pry'; binding.pry
+    assert_equal [], @reader_manager.collect_translation_keys(single_row)
+  end
+
+  def test_it_can_write_output_file
+    output_file_name = "reader_test.txt"
+    content = "welcome to my program!"
+    english = ".00.0.000.000....00...0000..000.0.000.0.00..\n00.00....0...0..00.0.....0..0.00.00000....00\n.0..0...0.0.....0.0...0.00..0.0.0...0...0.0."
+
+    assert output_file_name, @reader_manager.write_output_file(output_file_name)
+    assert_equal english, File.read(output_file_name)
   end
 end
