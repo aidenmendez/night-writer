b6044f13f83783ab380b4d14394e184228a1012b
Aiden Mendez <aidenmendez@gmail.com>
2020-11-09 19:07:25 -0700

    Update runner files

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,4 +2,4 @@ require "./lib/controller"
 
 user_input = ARGV
 controller = Controller.run(user_input, "writer")
-puts controller.confirm_file_created(controller.output_file_name, controller.manager.text.length)
+puts controller.confirm_file_created(controller.output_file_name, controller.manager.input_file_content.length)
